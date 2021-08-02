7e399f4afc8e318a8b4b97f0a76d8d8b19b93c34
Gerry Sheva Indianto <gshevvv@gmail.com>
2021-08-02 15:56:29 +0700

    mas witen

--- components/KesenianCard.js
+++ components/KesenianCard.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import Image from 'next/image'
-import { Col, Card, Button } from 'react-bootstrap'
+import Link from 'next/link'
+import styles from './KesenianCard.module.scss'
 
 export default function KesenianCard({kesenian}) {
     const { thumbnail, judul, slug} = kesenian.fields
     console.log(kesenian)
     return (
-        <Col xs={6} md={4} id ="card">
-            <Card className="text-center">
-                <Card.Img src={'https:' + thumbnail.fields.file.url}/>
-                <Card.Body>
-                    <Card.Title>{judul}</Card.Title>
-                    <Button href={'/kesenian/' + slug }>Temukan lebih lanjut</Button>
-                </Card.Body>
-            </Card>
-        </Col>
+        <div className={styles.container}>
+            <div className={styles.img}>
+                <Image 
+                src={'https:' + thumbnail.fields.file.url} 
+                height={thumbnail.fields.file.details.image.height} 
+                width={thumbnail.fields.file.details.image.height} 
+                alt='Foto Kesenian' layout='intrinsic' />
+            </div>
+            <div className={styles.details}>
+                <h3>{ judul }</h3>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div className={styles.btn}>
+                    <Link href={'/kesenian/' + slug } passHref>
+                        <button> Lihat lebih detail</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
     )
 }
